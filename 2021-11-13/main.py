9b43a9baa48cd6e6e7b0a8229f830cd0964f2308
Shaun Leung <Shaun.Leung@gmail.com>
2021-11-13 22:11:44 -0500

    Solution for Nov 13, 2021

---
+++ 2021-11-13/main.py
@@ -0,0 +1,44 @@
+import random
+
+k = 17
+nums = [10,15,3,7]
+
+def sumChecker(nums, k):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i]+nums[j] == k:
+                return True
+    return False
+
+# positve check
+print(sumChecker(nums, k))
+
+# negative check
+nums = [1,15,3,7]
+print(sumChecker(nums, k))
+
+# empty list check
+k = 0
+nums = []
+print(sumChecker(nums, k))
+
+# empty list check
+k = 17
+nums = []
+print(sumChecker(nums, k))
+
+# random check
+
+# Generate a random list of numbers
+def generateRandomList(n):
+    nums = []
+    for i in range(n):
+        nums.append(random.randint(1,10))
+    return nums
+
+k = random.randint(1,10)
+nums = generateRandomList(25)
+print('k = ' + str(k))
+print ('list = ' + str(nums))
+print(sumChecker(nums, k))
+
